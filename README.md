ecd87b6b7078b7536ebf0eaecf6c994af7c19d83
zenbar <victortse93@gmail.com>
2015-06-26 16:22:52 -0700

    Bug fixes
    missing semi-colon
    mis-spell function name
    Readme documenation

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ for self-checking their submission.
 How this game work:
 1) player start at the base location
 2) player's goal is to move to the top row without being hit by enemy
-3) if player move to the top row, he win and score is increase by one. player put back at starting point
-4) if player hit by enemy, he lose and score decrease by one. player put back at starting point
-5) when player score over 5 points, the speed of enemy increase. when player score over 10 points,
+3) player move by using the up, down, right arrow and left arrow key
+4) if player move to the top row, he win and score is increase by one. player put back at starting point
+5) if player hit by enemy, he lose and score decrease by one. player put back at starting point
+6) when player score over 5 points, the speed of enemy increase. when player score over 10 points,
    the speed increase again
    
+To play the game, visit: http://kendrickt.com/frogger
--- js/app.js
+++ js/app.js
@@ -3,29 +3,29 @@ var speed = 120;
 
 // Enemies our player must avoid
 var Enemy = function() {
-    
+
     // Variables applied to each of our instances go here,
     this.x = 0;
     this.y = 0;
-    
+
     // The image/sprite for our enemies
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position
 Enemy.prototype.update = function(dt) {
-    
-    // randomize the start of enemy so they do not always all start at the same time 
+
+    // randomize the start of enemy so they do not always all start at the same time
     var start = 1;
     if (this.x == 0) {
         if (Math.floor(Math.random() * 1000) < 980)
             start = 0;
     }
-    
+
     if (start == 1) {
         // change the speed when player score more than 5 pts or 10 pts
         // make the game a bit more challenging as the game progress
-        changeSpeed();
+        this.changeSpeed();
         this.x += dt * speed;
         // after crossing the line, reset and randomly pick a new row to start
         if (this.x > 101*4){
@@ -34,12 +34,12 @@ Enemy.prototype.update = function(dt) {
             this.y = (Math.floor(Math.random() * 3) + 1) * 83;
         }
     }
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 var Player = function() {
     this.sprite = 'images/char-princess-girl.png';
@@ -50,59 +50,59 @@ var Player = function() {
 
 Player.prototype.update = function(){
     // check if the player get hit by enemy. If so, decrease score
-    checkCollision();
-    // check if the player cross the road. If so, increase score    
-    checkWinning();
-}
+    this.checkCollision();
+    // check if the player cross the road. If so, increase score
+    this.checkWinning();
+};
 
-function checkCollision(){
+Player.prototype.checkCollision = function(){
     for (var i=0; i<NUM_BUGS; i++){
         if ( Math.sqrt(Math.pow(player.x - allEnemies[i].x, 2) + Math.pow(player.y - allEnemies[i].y, 2)) < 60) {
             // collision! put player back at starting point
             player.x = 101*2;
             player.y = 83*5;
             player.score -= 1;
-            updateScore();
+            this.updateScore();
         }
     }
-}
+};
 
-function checkWinning() {
+Play.prototype.checkWinning = function() {
     // if the player get to the top row, he win and move him back to starting point
     if (player.y == 0) {
         player.score += 1;
         player.x = 101*2;
         player.y = 83*5;
-        updateScore();
+        this.updateScore();
     }
-}
+};
 
 // update score in the HTML
-function updateScore() {
+Play.prototype.updateScore = function() {
     var score = "Score: " + player.score.toString();
-    document.getElementById("score").innerHTML = score;    
-}
+    document.getElementById("score").innerHTML = score;   
+};
 
 // change the speed of enemy to make the game more interesting
-function changSpeed() {
+Play.prototype.changeSpeed = function() {
     if ( player.score >5 && plaayer.score <10)
         speed = 170;
     if (player.score >= 10) {
         speed = 200;
     }
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // move the player
 Player.prototype.handleInput = function(key) {
     switch (key)
-    {   
-        case "left":          
+    {
+        case "left":
             if (this.x >= 101) {
-                this.x -= 101; 
+                this.x -= 101;
             }
             break;
         case "right":
@@ -113,18 +113,18 @@ Player.prototype.handleInput = function(key) {
         case "up":
             if (this.y >= 83) {
                 this.y -= 83;
-            }     
+            }
             break;
         case "down":
             if (this.y <= 83*4) {
                 this.y += 83;
-            } 
+            }
             break;
     }
-}
+};
 
 allEnemies = [];
-const NUM_BUGS= 3;  
+var NUM_BUGS= 3;
 for (var i=0; i<NUM_BUGS; i++){
     enemy = new Enemy();
     // put it on a random row
